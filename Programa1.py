251b78e64dc7c493b4387acf457dafa473da5a42
ElGiohhh <za210120419@zapopan.tecmm.edu.mx>
2024-01-30 10:09:03 -0600

    programa1

---
+++ Programa1.py
@@ -0,0 +1,13 @@
+
+#programa1 N Y A
+
+# Edgar Giovanni Oliveros Jimenez
+texto="""Nombre
+Edgar Giovanni Oliveros Jimenez"""
+texto2="""Numero de Control
+210120419"""
+
+
+print(texto)
+
+print(texto2)
